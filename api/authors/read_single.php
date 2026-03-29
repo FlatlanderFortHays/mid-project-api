1df042da02bbb9c1fb9b45fe1dd3d68a87eb3508
Patrick <patman57@gmail.com>
2026-03-29 15:06:23 -0500

    Fix read_single null object error

--- api/authors/read_single.php
+++ api/authors/read_single.php
@@ -1,18 +1,17 @@
 <?php
-// Get ID from URL parameters
-$category->id = isset($_GET['id']) ? $_GET['id'] : die();
+// Get ID from URL
+$author->id = isset($_GET['id']) ? $_GET['id'] : die();
 
-// Get the category details
-if($category->read_single()) {
-    // Create array for JSON output
-    $cat_arr = array(
-        'id' => $category->id,
-        'category' => $category->category
+// Get author
+if($author->read_single()) {
+    // Create array
+    $author_arr = array(
+        'id' => $author->id,
+        'author' => $author->author_name
     );
 
-    // Output JSON
-    echo json_encode($cat_arr);
+    // Make JSON
+    echo json_encode($author_arr);
 } else {
-    // Required error message if the ID does not exist
-    echo json_encode(array('message' => 'category_id Not Found'));
+    echo json_encode(array('message' => 'Author Not Found'));
 }
